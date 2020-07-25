722717e781ff39d4ffeb89e892d9ae29ad7b64ed
Diego Munera <dmejia-ctr@orbistechnologies.com>
2020-07-25 17:47:30 -0500

    Redux forms setup

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+  renderInput({ input }) {
+    return <input {...input} />;
+  }
 
-export default StreamCreate;
+  render() {
+    return (
+      <form>
+        <Field
+          name="title"
+          component={this.renderInput}
+          type="text"
+          placeholder="Title"
+        />
+        <Field
+          name="description"
+          component={this.renderInput}
+          type="text"
+          placeholder="Description"
+        />
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: "streamCreate",
+})(StreamCreate);
